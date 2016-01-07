6d32c8e34b3de7a782e08d9c8d99cbb2df6be3a4
Allen Kleiner <akleiner24@gmail.com>
2016-01-07 00:32:52 -0500

    Initial commit. Lots more to do! Bless up.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # khaled-quotes
-Inspiration from DJ Khaled, available in your browser at any time
+Khaled Quotes is a Chrome Extension that allows you to quote the 🔑 to success at any time. Currently a WIP. 
+
+Things to do: 
+
+1. Move quotes into a DB.
+2. Add more quotes, minimize quote collision.  
+3. Add a button? 
+4. Settings page. 
+5. ???
+6. Profit
---
+++ script.js
@@ -0,0 +1,47 @@
+var quotes = [
+  "they dont want u to eat !! i promise u !",
+  "bless up 🙏",
+  "lets see 👀",
+  "cloth talk comin soon",
+  "ride wit me on the pathway to more success",
+  "LIOOOONNNNNN",
+  "Another one. Another one. Another one. Another one.",
+  "i told my security its fan luv its all good",
+  "cocoa butter is the 🔑",
+  "egg whites, turkey sausage, water. they dont want u to eat",
+  "When you win big, you win more - they gonna sue you. Just counter sue 'em tho. Don't back down.",
+  "Rain will come, rain will go. When you a winner - rain on 'em forever.",
+  "Some people can't handle success. I can."
+];
+
+var handler = function(event) {
+  // CTRL + Shift + Y
+  if (event.ctrlKey && event.shiftKey && event.keyCode === 25) {
+    var quote = getSuccess();
+    copyQuoteToClipboard(quote);
+  }
+}
+
+var copyQuoteToClipboard = function(quote) { 
+  var area = createTextArea();
+  var oldFocus = document.activeElement;
+  area.value = quote; 
+  area.select();
+  document.execCommand("copy");
+  oldFocus.focus();
+  document.body.removeChild(area);
+}
+
+var createTextArea = function(id) {
+  var element = document.createElement("textarea");
+  element.style.left = "9999px";
+  element.id = "djk-textarea";
+  document.body.appendChild(element);
+  return element;
+}
+
+var getSuccess = function() { 
+  return quotes[Math.floor(Math.random() * quotes.length)];
+}
+
+document.addEventListener("keypress", handler, true);
