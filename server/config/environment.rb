a792edab69e5c7c3722936110a2842e77a798403
Allen Kleiner <akleiner24@gmail.com>
2016-01-08 23:46:28 -0500

    Server is a WIP

---
+++ server/config/environment.rb
@@ -0,0 +1,13 @@
+configure :production, :development do
+  # Database connection
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/quotes')
+ 
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
---
+++ server/server.rb
@@ -0,0 +1,41 @@
+require 'sinatra'
+require 'json'
+require 'yaml'
+require 'sinatra/activerecord'
+require "./config/environment"
+require './models/quote'
+
+helpers do
+  def protected!
+    return if authorized?
+    headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+    halt 401, "Why you snooping, fam?\n"
+  end
+
+  def authorized?
+    @config = YAML.load_file("config.yaml")
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == [@config["username"], @config["password"]]
+  end
+end
+
+get '/protected' do 
+    protected!
+    erb :admin
+end
+
+get '/quote' do
+    "Random quote test"
+end 
+
+post '/quote' do 
+    params[:quote]
+end
+
+get '/:quote' do 
+    params[:quote]
+end
+
+get '/' do 
+    'Server is up and running.'
+end
